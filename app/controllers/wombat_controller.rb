2797ba40c49a47f88e75a21433871b25479dcc29
Mykl Clason <myklclason@gmail.com>
2016-02-12 02:23:56 +0000

    Mostly working, just need to get all images

--- app/controllers/wombat_controller.rb
+++ app/controllers/wombat_controller.rb
@@ -1,7 +1,9 @@
 class WombatController < ApplicationController
 
   def index
-    @results = scrape
+    results = scrape
+    @tables = results["tables"]
+    @images = results["images"]
   end
 
   private
@@ -10,15 +12,30 @@ class WombatController < ApplicationController
   end
   def filter(results)
     tables = results["tables"]
+
+    # Should filter if heading is nil
     tables.delete_if { |content| content["heading"].nil?}
-    tables
+
+    # Should filter if value or attribute is nil
+    tables.each do |table|
+      rows = table["rows"]
+      rows.delete_if { | row | row["attr"].nil? || row["value"].nil? }
+    end
+
+    # Filter out any empty rows
+    tables.delete_if { |content| content["rows"].empty?}
+    results
   end
 
   def scrape_ingrammicro
     require 'wombat'
     Wombat.crawl do
       base_url "https://us-new.ingrammicro.com"
-      path "/_layouts/CommerceServer/IM/ProductDetails.aspx?id=US01@@9500@@10@@YW9103"
+      #path "/_layouts/CommerceServer/IM/ProductDetails.aspx?id=US01@@9500@@10@@YW9103"
+      path "/_layouts/CommerceServer/IM/ProductDetails.aspx?id=US01@@9500@@10@@YW8658"
+
+      images xpath: "//img/@src", :iterator do
+      end
 
       tables "css=table", :iterator do
         rows "css=tr"
